1cf33d7d6aa58335c70739ece443fc6e865463f8
asish <asish1@gmail.com>
2020-12-05 20:45:27 +0530

    pcommit

--- SpringDataREST3/src/main/java/com/asish/demo/repository/PerfumeRepository.java
+++ SpringDataREST3/src/main/java/com/asish/demo/repository/PerfumeRepository.java
@@ -6,7 +6,11 @@ import org.springframework.stereotype.Repository;
 
 import com.asish.demo.entity.Perfume;
 
+import io.swagger.annotations.Api;
+
+@Api("asish kumar")
 @RepositoryRestResource(collectionResourceRel = "asish",path = "asi")
 public interface PerfumeRepository extends PagingAndSortingRepository<Perfume, Integer> {
+	
 
 }
