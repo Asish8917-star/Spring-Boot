f735d261ba0ed0641990285ed5d331b9f5da7b0a
asish <asish1@gmail.com>
2021-02-11 09:53:10 +0530

    paypal

---
+++ PaymentIntegration/src/main/java/com/asish/demo/PaymentIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.asish.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentIntegrationApplication.class, args);
+	}
+
+}
